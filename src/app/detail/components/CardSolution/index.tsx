561cfd738760a99843dadd95c78e6d3738c3aeea
Rauane Lima <rauanelima7@gmail.com>
2023-11-27 18:59:33 -0300

    Bugfix: resolving typing errors

--- src/app/detail/components/CardSolution/index.tsx
+++ src/app/detail/components/CardSolution/index.tsx
@@ -1,7 +1,7 @@
 import Button from "@/components/Button";
 
 type CardSolutionProps= {
-  cardSolution: CardSolution;
+  cardSolution: SolutionVoted;
 }
 
 const CardSolution= ({ cardSolution}: CardSolutionProps) => {
